0b7cebaa6d92da27b5c63009f500651bde6443ea
HarmanChen <34202216+harmanchen@users.noreply.github.com>
2021-10-18 09:27:55 +0800

    Add files via upload

---
+++ lanes_video.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from lanes import canny, region_of_interest,\
+    average_slope_intercept, display_lines
+
+
+cap = cv2.VideoCapture("test2.mp4")
+while cap.isOpened():
+    _, frame = cap.read()
+    canny_image = canny(frame)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100,
+                            np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow("result", combo_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
